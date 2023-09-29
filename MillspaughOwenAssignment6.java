a30a14a88ccd2f0f8e26318cff6d770584d4b99c
Ashaman123 <odogb02@gmail.com>
2023-09-29 15:36:20 -0600

    Commit

--- MillspaughOwenAssignment6.java
+++ MillspaughOwenAssignment6.java
@@ -16,46 +16,50 @@ public class MillspaughOwenAssignment6 {
 	
 	System.out.println("Please enter how much money you would like to use ($1 = 1 credit): ");
 		int userCredits = scanner.nextInt();
+		
+		
 		while(userCredits > 0 && spin != "N") { //Outside while loop continues if userCredit > 0 && spin == 'Y'
-			//scanner.nextLine();
+			
 			System.out.println("Entered while loop");
 			
 			
 			for (int i = 0; i < userCredits; i++) { //spin roulette
+				//scanner.nextLine();
 				System.out.println("Entered for loop. i = " + i);
 				System.out.println("");
 					//word = random.nextInt(6); 						
-						word = random.nextInt(6); 
+						word = random.nextInt(5); 
 					switch (word){ //Assigns random int to a "image" for spinner
-						case 1:
+						case 0:
 							image = "PLUM";
 							System.out.println("Prints: " + image);
 							break;
-						case 2:
+						case 1:
 							image = "CHERRY";
 							System.out.println("Prints: " + image);
 	
 							break;
-						case 3: 
+						case 2: 
 							image = "BELL";
 							System.out.println("Prints: " + image);
 	
 							break;
-						case 4: 
+						case 3: 
 							image = "BAR";
 							System.out.println("Prints: " + image);
 	
 							break;
-						case 5: 
+						case 4: 
 							image = "SEVEN";
 							System.out.println("Prints: " + image);
+							 break;
 						default: 
-							System.out.println("I shouldnt be here");
 							break;
 							
 				}
 				
 			}
+			
 			System.out.println("Would you like to spin again?");
 				spin = scanner.nextLine().toUpperCase();
 				//System.out.println(userCredits);
